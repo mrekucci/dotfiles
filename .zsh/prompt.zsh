0fe0bc581ba435eaf7df95e8fac1df287f603713
Peter Mrekaj <mrekucci@gmail.com>
2016-08-21 15:00:18 +0200

    Enable staged/unstaged changes detection on zsh's vcs_info

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,6 +1,11 @@
 # Format the prompt to include useful information about actual git repository
 autoload -Uz vcs_info
-zstyle ':vcs_info:git*' formats "%r/%S [%b](%a) %m%u%c "
+zstyle ':vcs_info:git*' check-for-changes true
+zstyle ':vcs_info:git*' check-for-staged-changes true
+zstyle ':vcs_info:git*' stagedstr "●"
+zstyle ':vcs_info:git*' unstagedstr "●"
+zstyle ':vcs_info:git*' actionformats "%r/%S (%b|%F{5}%a%f) %F{1}%u%F{2}%c%f "
+zstyle ':vcs_info:git*' formats "%r/%S (%b) %F{1}%u%F{2}%c%f "
 precmd() { vcs_info }
 setopt prompt_subst
-PROMPT='${vcs_info_msg_0_}%# '
+PROMPT='${vcs_info_msg_0_}%\> '

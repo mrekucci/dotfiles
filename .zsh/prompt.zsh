d28ef2806cf0c22edffa407714c6cbee7a9474b5
Peter Mrekaj <mrekucci@gmail.com>
2017-07-11 10:16:58 +0200

    zsh: colorize shell symbol according to the previous renurn value

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -4,8 +4,9 @@ zstyle ':vcs_info:git*' check-for-changes true
 zstyle ':vcs_info:git*' check-for-staged-changes true
 zstyle ':vcs_info:git*' stagedstr "●"
 zstyle ':vcs_info:git*' unstagedstr "●"
-zstyle ':vcs_info:git*' actionformats "%r/%S (%b|%F{5}%a%f) %F{1}%u%F{2}%c%f "
-zstyle ':vcs_info:git*' formats "%r/%S (%b) %F{1}%u%F{2}%c%f "
+zstyle ':vcs_info:git*' actionformats "%r/%S (%b|%F{magenta}%a%f%F{default}) %F{red}%u%F{green}%c%f%F{default} "
+zstyle ':vcs_info:git*' formats "%r/%S (%b) %F{red}%u%F{green}%c%f%F{default} "
 precmd() { vcs_info }
 setopt prompt_subst
-PROMPT='${vcs_info_msg_0_}%\> '
+SYMBOL="%(!.#.>)" # TODO: root, and redefine colours on git
+PROMPT='${vcs_info_msg_0_}%(?.$SYMBOL.%F{red}$SYMBOL%F{default}) '

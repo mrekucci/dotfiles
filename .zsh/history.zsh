af78e4db14dc4ee3abe0e375ed2d5e5d3d3777da
Peter Mrekaj <mrekucci@gmail.com>
2018-08-31 16:39:42 +0200

    zsh: set new options in history
     - for removing command duplicates
     - for removing commands that start with space

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -2,7 +2,8 @@ STSIZE=5000
 HISTFILE=~/.zsh_history
 SAVEHIST=5000
 setopt appendhistory
-setopt sharehistory
 setopt incappendhistory
 setopt extendedglob
 setopt nomatch
+setopt histignoredups
+setopt histignorespace

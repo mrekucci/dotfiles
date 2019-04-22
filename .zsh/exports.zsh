19a071f4f9b8ca58117b8d2141acc392b6585477
Peter Mrekaj <mrekucci@gmail.com>
2019-04-22 14:49:06 +0200

    zsh: remove GOPATH

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,4 +1,2 @@
-# GOPATH environment variable for Golang
-export GOPATH=$HOME/Workspace/go/dependencies:$HOME/Workspace/go/projects
 # Enable color in grep
 export GREP_OPTIONS='--color=auto'
--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,10 +1,5 @@
+# Include binaries build/downloaded by Rust
 path+=($HOME/.cargo/bin)
-
-# Include directories from GOPATH in search when changing path
-for gopath in ${(s/:/)GOPATH}; do
-    path+=($gopath/bin) # Include binaries built by Golang in the search.
-    cdpath+=($gopath/src/golang.org $gopath/src/github.com $gopath/src/bitbucket.org)
-done
 
 # Automatically change path if typed word is a directory
 setopt auto_cd
